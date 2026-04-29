e28057732e9a5b24eba4bf2c3c68ea69561d0e9a
ramyzaherr <ramyzaher250@gmail.com>
2026-04-29 11:28:41 +0300

    model/User & model/Account

--- src/main/java/com/budgetapp/model/Account.java
+++ src/main/java/com/budgetapp/model/Account.java
@@ -1,5 +1,13 @@
 package com.budgetapp.model;
 
 public class Account {
+    private String email;
+    private String password;
+    private int userId;
+    public Account(String e,String p,int i){
+        email=e;
+        password=p;
+        userId=i;
+    }
     
 }
--- src/main/java/com/budgetapp/model/User.java
+++ src/main/java/com/budgetapp/model/User.java
@@ -1,5 +1,23 @@
 package com.budgetapp.model;
 
 public class User {
+    private int id;
+    private String Name;
+    private double Balance;
+    private String Currency;
+    public User(int i, String n,double b,String c){
+        id=i;
+        Name=n;
+        Balance=b;
+        Currency=c;
+    }
+
+    public boolean updateBalance(double amount){
+        if(Balance+amount>=0){
+            Balance+=amount;
+            return true;
+        }
+        return false;
+    }
     
 }
